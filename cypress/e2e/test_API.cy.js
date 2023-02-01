555b6b3b0c26bffa75741a7f0a8106890ee85b03
FIRST\_Mahfoud LAST\_HANI <[MY\_NAME@example.com](mailto:hanimahfoud@gmail.com)>
2023-02-01 11:21:17 +0100

    réctification

--- cypress/e2e/test_API.cy.js
+++ cypress/e2e/test_API.cy.js
@@ -1,31 +1,29 @@
 // <reference types="cypress" />
 
-describe('template spec', () => {
-  beforeEach(() => {
-    
-
-      cy.fixture('datas.json').as('userdata');
-   
-   
-  })
+describe('Vérification the API', () => {
+  
   it('Test_API', () => {
+    cy.fixture('datas.json').as('userdata');
     
     cy.get('@userdata').then(datas => {
-      datas.forEach(user => {
-        
-        cy.request(
-          `https://tastedive.com/api/similar?q=${user.Name}`
-        ).then(resp => {
-          expect(resp.status).to.equal(200);
-          expect(resp.body.Similar).to.have.property('Info');
-        })
-
-        
-      });
+      datas.forEach(user => { 
+          cy.apisimilartest(`${user.Name}`)
+          
+          .then(resp => {
+            expect(resp.status).to.equal(200);
+            expect(resp.body.Similar).to.have.property("Info");
+            expect(resp.body.Similar).to.have.property("Results");
+            expect(resp.body.Similar.Results[0]).to.not.be.null;
+            expect(resp.body.Similar.Results).to.have.length(20);
+            expect(resp.body.Similar.Info).to.have.length(1);
+          })
+         
+          
+        });
+      })
+      
+      
     })
 
-
-})
-
-
-})
+  })
+  
